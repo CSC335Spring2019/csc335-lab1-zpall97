082d12332fd989c6b9a2655014187c5608994a9b
Zelin <Zelin@dhcp-10-134-252-82.uawifi.arizona.edu>
2019-01-16 09:01:02 -0700

    Added in the parts that we needed according the comments

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,14 +1,14 @@
 /**
  * 
- * @author YOUR NAME HERE
- * 
+ * Zelin Pallanes
+ * CSC 335 Lab
  * 
  */
 public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
-		System.out.println("Hello, World!");
+		System.out.println("Hello CSC 335, I'm Zelin Pallanes");
 	}
 
 }
